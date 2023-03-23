cff951d15e13bdaff1b8be488cee9fd7d031d157
Seyed Mahdi Hosseini Zade <mahdihosseinizade2000@gmail.com>
2023-03-24 02:53:47 +0430

    make challenge7 better

--- challenge7.js
+++ challenge7.js
@@ -1,154 +1,142 @@
 const warehouse = {
     products: [
-        {id: 1, name: 'Shoes', count: 10, price: 120, enabled: true},
-        {id: 2, name: 'Socks', count: 5, price: 15, enabled: true},
-        {id: 3, name: 'Jeans', count: 0, price: 320, enabled: false},
+        { id: 1, name: "Shoes", count: 10, price: 120, enabled: true },
+        { id: 2, name: "Socks", count: 5, price: 15, enabled: true },
+        { id: 3, name: "Jeans", count: 0, price: 320, enabled: false },
+        { id: 4, name: "Coat", count: 36, price: 456, enabled: false },
+        { id: 5, name: "Cap", count: 25, price: 64, enabled: true },
     ],
-    soldProduct : []
+    soldProducts: [],
 };
-function showAllProducts(products){
+function showAllProducts(products) {
     for (const item of products) {
         console.log(item);
     }
 }
-// - Write a function to get the count of all the items in the warehouse combined. (item1 count + item2 count + ...).
 function getTotalCount() {
-    let totalCount = 0;
-    for (let i = 0; i < warehouse.products.length; i++) {
-        totalCount += warehouse.products[i].count;
-    }
-    return totalCount;
+    console.log(`The total count in warehouse is ${ warehouse.products.reduce(
+        (totalCount, product) => totalCount + product.count,0)
+    }`);
 }
-
-// --------Declare a function to create a new item with the given count.
-function createNewItem(name,count,price,enabled){
-    const id = warehouse.products.length + 1
-    const newItem = {
-        id ,
-        name ,
-        count,
-        price,
-        enabled,
-    }
+function getNextProductId() {
+    return warehouse.products.length + 1;
+}
+function createNewItem(name, count, price, enabled) {
+    const id = getNextProductId();
+    const newItem = { id, name, count, price, enabled };
     warehouse.products.push(newItem);
-    console.log('newItem created !');
-    // showAllProducts();
+    console.log("New item created!");
+    showAllProducts(warehouse.products);
 }
-// ----We need to have a function for selling the items.
-function sellItem (productId,quantity) {
-    const index = warehouse.products.findIndex((item) => item.id === productId)
-    const product = {...warehouse.products[index]}
-    if (product.count >= quantity ) {
+function sellItem(productId, quantity) {
+    const product = warehouse.products.find(
+        (product) => product.id === productId
+    );
+    if (product && product.count >= quantity) {
         product.count -= quantity;
-        warehouse.soldProduct.push({id:productId,quantity:quantity,price:warehouse.products[index].price});
-        console.log(warehouse.soldProduct);
-        const AllItem = [...warehouse.products]
-        AllItem[index] = product;
-        console.log(`the Item sell is :${product.name} and the quantity saled is : ${quantity}`);
-        showAllProducts(AllItem);
-    }
-    else{
-        console.log('Unfortunately, the number of your selection is more than the products in stock');
-    }
-}
-
-// ------- Write down a function to give a list of all the items that they are available (their count is above 0)
-function listOfAvailable(){
-    const availableProduct = []
-    for (let i = 0; i < warehouse.products.length; i++) {
-        if (warehouse.products[i].count > 0) {
-            availableProduct.push(warehouse.products[i]);
-        }        
-    }
-    if (availableProduct.length > 0) {
-        showAllProducts(availableProduct);
-    }else{
-        console.log('there is not any product available');
+        warehouse.soldProducts.push({
+            id: productId,
+            quantity,
+            price: product.price,
+        });
+        console.log(
+            `Sold ${quantity} ${product.name} .totalCost is : ${quantity * product.price}$.`
+        );
+        showAllProducts(warehouse.products);
+    } else {
+        console.log(
+            `Unable to sell ${quantity} of ${
+                product ? product.name : "unknown product"
+            } (not enough stock).`
+        );
     }
 }
-
-// ---------- Write down a function to disable/enable an item from the warehouse.
-
-function enableProduct(productId) {
-    for (let i = 0; i < warehouse.products.length; i++) {
-        if (warehouse.products[i].id === productId) {
-            warehouse.products[i].enabled = true;
-            console.log('The Item is now available');
-        }
+function listAvailableProducts() {
+    const availableProducts = warehouse.products.filter(
+        (product) => product.count > 0
+    );
+    if (availableProducts.length > 0) {
+        showAllProducts(availableProducts);
+    } else {
+        console.log("There are no available products.");
     }
-    showAllProducts(warehouse.products);
-    return false;
 }
-function disableProduct(productId) {
-    for (let i = 0; i < warehouse.products.length; i++) {
-        if (warehouse.products[i].id === productId) {
-            warehouse.products[i].enabled = false;
-            console.log('The Item is now Un');
-        }
+function toggleProductEnabled(productId, isEnabled) {
+    const product = warehouse.products.find(
+        (product) => product.id === productId
+    );
+    if (product) {
+        product.enabled = isEnabled;
+        console.log(
+            `The item "${product.name}" is now ${
+                isEnabled ? "enabled" : "disabled"
+            }.`
+        );
+        showAllProducts(warehouse.products);
+    } else {
+        console.log("Product not found.");
     }
-    showAllProducts(warehouse.products);
-    return false;
 }
-
-// ------- - Write down a function to change the count of an item from the warehouse.
-
- 
-function changeProductCount(productId,newCount) {
-    const productIndex = warehouse.products.findIndex((product) => product.id ===productId);
-    if (productIndex === -1) {
-        console.log('product not found 404');
-        return;
+function changeProductCount(productId, newCount) {
+    const product = warehouse.products.find(
+        (product) => product.id === productId
+    );
+    if (product) {
+        product.count = newCount;
+        console.log(
+            `The count of "${product.name}" has been changed to ${newCount}.`
+        );
+        showAllProducts(warehouse.products);
+    } else {
+        console.log("Product not found.");
     }
-     const product =  warehouse.products[productIndex]
-     product.count = newCount;
-     console.log('product count changed ');
-     showAllProducts(warehouse.products);
 }
-
-// - We need to have a function to edit the item from the warehouse for example their name.
-
-function editProduct(productId,newName) {
-    const productIndex = warehouse.products.findIndex((product) => product.id ===productId);
-    if (productIndex === -1) {
-        console.log('product not found 404');
-        return;
+function editProduct(productId, newName, newCount, newPrice, isEnabled) {
+    const productIndex = warehouse.products.findIndex(
+        (product) => product.id === productId
+    );
+    if (productIndex !== -1) {
+        const product = warehouse.products[productIndex];
+        product.name = newName !== undefined ? newName : product.name;
+        product.count = newCount !== undefined ? newCount : product.count;
+        product.price = newPrice !== undefined ? newPrice : product.price;
+        product.enabled = isEnabled !== undefined ? isEnabled : product.enabled;
+        console.log(`Product with id ${productId} has been updated:`);
+        console.log(product);
+    } else {
+        console.log(`Product with id ${productId} not found.`);
     }
-     const product =  warehouse.products[productIndex]
-     product.name = newName;
-     console.log('product name changed ');
-     showAllProducts(warehouse.products);
 }
-
-// Declare a function to calculate price of all the items that have been sold.
-//  (For this you need to store the items that have been sold in an array or something)
-
-function caclPriceOfSoldItem(){
+function calculateSoldItemsPrice() {
+    sellItem(2,2)
     let totalPrice = 0;
-    warehouse.soldProduct.forEach((product) => totalPrice += (product.price * product.quantity))
-    console.log('totalPrice is' , totalPrice);
-    return totalPrice;
+    for (const soldItem of warehouse.soldProducts) {
+        totalPrice += soldItem.quantity * soldItem.price;
+    }
+    console.log(`The total price of sold items is ${totalPrice}.`);
 }
-
-// We need to check the warehouse's value by calculating the price of all the available items. 
-function availableProductValue(){
-    let availableProduct = []
-    let cost = 0 ;
-    for (let i = 0; i < warehouse.products.length; i++) {
-        if (warehouse.products[i].enabled) {
-            availableProduct.push(warehouse.products[i]);
+function calculateWarehouseValue() {
+    let totalValue = 0;
+    for (const availableItem of warehouse.products) {
+        if (availableItem.count > 0) {
+            totalValue += availableItem.count * availableItem.price;
         }
     }
-    for (let i = 0; i < availableProduct.length; i++) {
-        cost += availableProduct[i].price
-    }
-    console.log(cost);
+    console.log(
+        `The total value of available items in the warehouse is ${totalValue}$.`
+    );
 }
-// availableProductValue();
-// sellItem(1,2)
-// createNewItem('Socks2',60,125,true)
-// console.log(getTotalCount());
-// disableProduct(3)
-// listOfAvailable();
-// changeProductCount(2,85)
-// editProduct(2,'mmd')
-// caclPriceOfSoldItem()
+
+
+// showAllProducts(warehouse.products);
+// getTotalCount();
+// createNewItem('T-shirt',45,69,false);
+// sellItem(5,24);
+// sellItem(5,26);
+// listAvailableProducts();
+// toggleProductEnabled(1,false)
+// changeProductCount(2,6)
+// changeProductCount(8,12)
+// editProduct(1,'shoes ',18,undefined ,false)
+// calculateSoldItemsPrice()    
+calculateWarehouseValue();
