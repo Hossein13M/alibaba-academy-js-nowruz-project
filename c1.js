f95bc61b5014fa08ee4ce17443f1ec9b4fe03a95
ahmadreza tahmasbi <ahmadreza.tmb@gmail.com>
2023-03-24 16:53:31 +0430

    add log for c1

--- c1.js
+++ c1.js
@@ -3,16 +3,24 @@
 // solution 1
 
 function printAllPrimes(){
+    var primes = []
     for(var i = 2 ; i <= 100 ; i++){
+        console.log(`analysing ${i}`);
         for(var j = 2 ; j < i ; j++){
             if (i % j == 0){
+                console.log(`${i} is not prime because of : ${i} % ${j} == 0`);
                 break;
             }
         }
         if(j == i){
-            console.log(i);
+            console.log(`${i} is prime !`);
+            primes.push(i);
         }
     }
+    return primes;
 }
 
-printAllPrimes()
+var primes = printAllPrimes()
+for(var prime of primes){
+    console.log(prime)
+}
