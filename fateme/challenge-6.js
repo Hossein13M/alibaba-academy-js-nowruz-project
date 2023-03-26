27c2e86884cfa3657bffe33afc15b1fc3c115dc4
fateme ebrahimzadeh <fateme.ebrahimzadeh20@gmail.com>
2023-03-26 11:11:17 +0430

    get rid of process for logging

--- fateme/challenge-6.js
+++ fateme/challenge-6.js
@@ -1,12 +1,13 @@
 function pyramid(n = 5) {
   for (let index = 1; index <= n; index++) {
+    const line = []
     for (let j = 1; j <= n - index; j++) {
-      process.stdout.write(" ");
+      line.push(" ")
     }
     for (let k = 1; k <= 2 * index - 1; k++) {
-      process.stdout.write("*");
+      line.push("*")
     }
-    console.log();
+    console.log(line.join(""));
   }
 }
 
