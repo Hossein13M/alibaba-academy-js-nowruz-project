93c8636270995d1079ec8f30770183ad8564d1c2
ahmadreza tahmasbi <ahmadreza.tmb@gmail.com>
2023-03-29 12:02:26 +0430

    change function name and return

--- Ahmadreza-tahmasebi/c2.js
+++ Ahmadreza-tahmasebi/c2.js
@@ -1,22 +1,22 @@
 "use strict"
 
-function checkRgbValue(...args){
+function isRgbValueOk(...args){
     for(const arg of args){
         if (!(arg >= 0 && arg<256)){
-            return 400;
+            return false;
         } 
     }
-    return 200;
+    return true;
 }
 
 
 function rgbToHex(r, g, b) {
-    if(checkRgbValue(r,g,b) != 200){
+    if(isRgbValueOk(r,g,b)){
         return "error"
     }
-    var digitOneTwo = (r.toString(16).length) == 2 ? r.toString(16) : `0${r.toString(16)}`;
-    var digitThreeFour = (g.toString(16).length) == 2 ? g.toString(16) : `0${g.toString(16)}`;
-    var digitFiveSix = (b.toString(16).length) == 2 ? b.toString(16) : `0${b.toString(16)}`;
+    var digitOneTwo = (r.toString(16).length) === 2 ? r.toString(16) : `0${r.toString(16)}`;
+    var digitThreeFour = (g.toString(16).length) === 2 ? g.toString(16) : `0${g.toString(16)}`;
+    var digitFiveSix = (b.toString(16).length) === 2 ? b.toString(16) : `0${b.toString(16)}`;
     return  digitOneTwo + digitThreeFour + digitFiveSix
 }
 
