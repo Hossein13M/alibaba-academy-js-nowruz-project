6cb8309519aa4f14983d93555680a4ef2e6104de
ahmadreza tahmasbi <ahmadreza.tmb@gmail.com>
2023-03-29 11:46:43 +0430

    change function name

--- Ahmadreza-tahmasebi/c2.js
+++ Ahmadreza-tahmasebi/c2.js
@@ -1,25 +1,25 @@
 "use strict"
 
-function RGBToHex(R, G, B) {
+function rgbToHex(r, g, b) {
     var error = []
-    if (!(R >= 0 && R<256)){
+    if (!(Rr >= 0 && r<256)){
         error.push("bad parameter for R");
     }
-    if (!(G >= 0 && G<256)){
+    if (!(g >= 0 && g<256)){
         error.push("bad parameter for G");
     }
-    if (!(B>= 0 && B<256)){
+    if (!(b>= 0 && b<256)){
         error.push("bad parameter for B");
     }
 
     if (error.length != 0){
         return {"error" : error, "status" : "400"}
     }
-    var digitOneTwo = (R.toString(16).length) == 2 ? R.toString(16) : `0${R.toString(16)}`;
-    var digitThreeFour = (G.toString(16).length) == 2 ? G.toString(16) : `0${G.toString(16)}`;
-    var digitFiveSix = (B.toString(16).length) == 2 ? B.toString(16) : `0${B.toString(16)}`;
+    var digitOneTwo = (r.toString(16).length) == 2 ? r.toString(16) : `0${r.toString(16)}`;
+    var digitThreeFour = (g.toString(16).length) == 2 ? g.toString(16) : `0${g.toString(16)}`;
+    var digitFiveSix = (b.toString(16).length) == 2 ? b.toString(16) : `0${b.toString(16)}`;
     return {"code" : digitOneTwo + digitThreeFour + digitFiveSix, "status" : 200} ;
 }
 
-var hex = RGBToHex(255,0,255);
+var hex = rgbToHex(255,0,255);
 console.log(hex)
