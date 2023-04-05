b526d778deae0be358512f1a190ff7870261f5a6
BenAB98 <96391742+BenAB98@users.noreply.github.com>
2023-04-05 10:03:14 +0330

    Delete index 2.js

--- index 2.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-function rgbaToHex(red, green, blue) {
-    return (red.toString(16) + (green.toString(16)) + blue.toString(16))
-}
-
-const hexaDecimal = rgbaToHex(53, 96, 243);
-
-console.log("#" + hexaDecimal);
